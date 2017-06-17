3996c99db8ab87dfc26d2ef0a68f3cbfbdf967b6
Kirill Golodenko <kirill@kirill.local>
2017-06-17 15:23:30 +0300

    header aligned

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,6 +8,6 @@ import Link from "gatsby-link"
 export default () =>
     <div>
         <h1>Kirill`s personal homepage</h1>
-        I like to make beautiful sites and their mobile versions. I pay special attention to functionality, usability and appearance. I think that a small departure from the strict canons of layout is quite acceptable for the sake of external "pleasantness" and ergonomics, especially for tablet versions and mobile applications. And I really love coffee;)
+        <p style={{width:"200px"}}>I like to make beautiful sites and their mobile versions. I pay special attention to functionality, usability and appearance. I think that a small departure from the strict canons of layout is quite acceptable for the sake of external "pleasantness" and ergonomics, especially for tablet versions and mobile applications. And I really love coffee;)</p>
         <Link to="">Home</Link>
     </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,15 @@ import Link from "gatsby-link"
 
 export default () =>
     <div>
-        <h1>Kirill`s personal homepage</h1>
-        <img style={{width: "400px"}} src="https://scontent.xx.fbcdn.net/v/t1.0-9/16473596_866135486861850_3048239217832092488_n.jpg?oh=51058c083194641b55f9f35c1f69255d&oe=59DDD577" />
-        <Link to="/about/">About</Link>
+        <header style={{"display":"flex",
+            "width": "500px",
+            "flex-direction": "row",
+            "justify-content": "space-around"}}>
+            <Link to="/about/">About</Link>
+            <Link to="">Home</Link>
+        </header>
+        <div>
+            <h1>Kirill`s personal homepage</h1>
+            <img style={{width: "400px"}} src="https://scontent.xx.fbcdn.net/v/t1.0-9/16473596_866135486861850_3048239217832092488_n.jpg?oh=51058c083194641b55f9f35c1f69255d&oe=59DDD577" />
+        </div>
     </div>
